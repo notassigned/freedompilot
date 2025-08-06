772e175f90cb0af4c51a58aeab466ee0c25e4749
notassigned <22502000+notassigned@users.noreply.github.com>
2025-08-05 21:53:36 -0400

    remove seatbelt requirement. death is not the worst of evils

--- selfdrive/car/car_specific.py
+++ selfdrive/car/car_specific.py
@@ -134,8 +134,6 @@ class CarSpecificEvents:
 
     if CS.doorOpen:
       events.add(EventName.doorOpen)
-    if CS.seatbeltUnlatched:
-      events.add(EventName.seatbeltNotLatched)
     if CS.gearShifter != GearShifter.drive and (extra_gears is None or
        CS.gearShifter not in extra_gears):
       events.add(EventName.wrongGear)
